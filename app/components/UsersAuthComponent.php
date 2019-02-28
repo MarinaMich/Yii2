64e566497a77294523ec057982f1703657105c7e
Talisman <a.egorov@nativetech.ru>
2019-02-28 20:12:15 +0300

    Авторизация

--- app/components/UsersAuthComponent.php
+++ app/components/UsersAuthComponent.php
@@ -31,6 +31,45 @@ class UsersAuthComponent extends Component
      * @param $model Users
      * @return bool
      */
+    public function loginUser(&$model):bool{
+        $user=$this->getUserByEmail($model->email);
+        if(!$user){
+            $model->addError('email','Пользователя не существует');
+            return false;
+        }
+
+        if(!$this->validatePassword($model->password,$user->password_hash)){
+            $model->addError('password','Пароль неверный');
+            return false;
+        }
+        $user->username=$user->email;
+
+        return \Yii::$app->user->login($user);
+    }
+
+    /**
+     * @param $password
+     * @param $hash
+     * @return bool
+     */
+    private function validatePassword($password,$hash){
+        return \Yii::$app->security->validatePassword($password,$hash);
+    }
+
+    /**
+     * @param $email
+     * @return Users|array|\yii\db\ActiveRecord
+     */
+    public function getUserByEmail($email){
+        return $this->getModel()::find()->andWhere(['email'=>$email])->one();
+    }
+
+//    public
+
+    /**
+     * @param $model Users
+     * @return bool
+     */
     public function createNewUser(&$model):bool{
         if(!$model->validate(['password','email'])){
             return false;
--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -14,6 +14,22 @@ use yii\web\Controller;
 
 class AuthController extends Controller
 {
+
+    public function actionSignIn(){
+        /** @var UsersAuthComponent $comp */
+        $comp=\Yii::$app->auth;
+
+        $model=$comp->getModel(\Yii::$app->request->post());
+
+        if(\Yii::$app->request->isPost){
+            if($comp->loginUser($model)){
+                return $this->redirect(['/activity/create'],200);
+            }
+        }
+
+        return $this->render('signin',['model'=>$model]);
+    }
+
     public function actionSignUp(){
         /** @var UsersAuthComponent $comp */
         $comp=\Yii::$app->auth;
